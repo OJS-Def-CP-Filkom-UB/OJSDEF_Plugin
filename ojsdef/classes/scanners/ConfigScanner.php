13524e4e75bcf77c77fb279fabd960bf0c15de6a
Aftaza <taftazani17@student.ub.ac.id>
2026-06-03 08:12:45 +0700

    fix(c1): ConfigScanner reads correct OJS keys (display_errors, show_stacktrace, smtp_enabled)

--- ojsdef/classes/scanners/ConfigScanner.php
+++ ojsdef/classes/scanners/ConfigScanner.php
@@ -16,16 +16,18 @@ class ConfigScanner
     public function scan(): array
     {
         return [
-            'debug_mode'            => (bool) $this->_cfg('debug',    'debug_mode',    false),
-            'show_errors'           => (bool) $this->_cfg('debug',    'show_errors',   false),
+            'display_errors'        => (bool) $this->_cfg('debug',    'display_errors',  false),
+            'show_errors'           => (bool) $this->_cfg('debug',    'show_errors',     false),
+            'show_stacktrace'       => (bool) $this->_cfg('debug',    'show_stacktrace', false),
             'api_key_secret_length' => strlen((string) $this->_cfg('general',  'api_key_secret', '')),
-            'force_ssl'             => (bool) $this->_cfg('security', 'force_ssl',     false),
-            'allowed_hosts_set'     => !empty($this->_cfg('security', 'allowed_hosts', '')),
-            'installed'             => (bool) $this->_cfg('general',  'installed',     false),
-            'smtp_auth_enabled'     => !empty($this->_cfg('email',    'smtp_auth',     '')),
-            'smtp_password_set'     => !empty($this->_cfg('email',    'smtp_password', '')),
-            'db_driver'             => (string) $this->_cfg('database', 'driver',      ''),
-            'db_host'               => (string) $this->_cfg('database', 'host',        ''),
+            'force_ssl'             => (bool) $this->_cfg('security', 'force_ssl',       false),
+            'allowed_hosts_set'     => !empty($this->_cfg('security', 'allowed_hosts',   '')),
+            'installed'             => (bool) $this->_cfg('general',  'installed',       false),
+            'smtp_enabled'          => (bool) $this->_cfg('email',    'smtp',            false),
+            'smtp_auth_enabled'     => !empty($this->_cfg('email',    'smtp_auth',       '')),
+            'smtp_password_set'     => !empty($this->_cfg('email',    'smtp_password',   '')),
+            'db_driver'             => (string) $this->_cfg('database', 'driver',        ''),
+            'db_host'               => (string) $this->_cfg('database', 'host',          ''),
             'db_password_empty'     => $this->_isDbPasswordEmpty(),
         ];
     }
